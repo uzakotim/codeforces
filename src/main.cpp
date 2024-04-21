509cd06e5d3580d49b64bc425a8805e063a11719
uzakotim <timauzakov@gmail.com>
2024-04-21 14:24:58 +0500

    clock conversion

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,56 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <vector>
 
-void drawChessBoard(int n)
+char *convertTime(const char *line)
 {
-    char result[2 * n][2 * n];
-    int counter_horizontal = 0;
-    int counter_vertical = 0;
-    for (int i = 0; i < 2 * n; i++)
-    {
-        counter_horizontal = 0;
-        if (counter_vertical % 4 == 0)
-            counter_vertical = 0;
-        // PRINT ROW
-        if (counter_vertical < 2)
-        {
-            for (int j = 0; j < 2 * n; j++)
-            {
-                if (counter_horizontal % 4 == 0)
-                {
-                    result[i][j] = '#';
-                    counter_horizontal = 0;
-                }
-                if (counter_horizontal < 2)
-                {
-                    result[i][j] = '#';
-                }
-                else
-                {
+    int hours = 0;
+    int minutes = 0;
 
-                    result[i][j] = '.';
-                }
-                counter_horizontal++;
-            }
-        }
-        else
-        {
-            // PRINT REVERSE ROW
-            for (int j = 0; j < 2 * n; j++)
-            {
-                if (counter_horizontal < 2)
-                {
-                    result[i][j] = '.';
-                }
-                else
-                {
+    hours = (line[0] - '0') * 10 + (line[1] - '0');
+    minutes = (line[3] - '0') * 10 + (line[4] - '0');
 
-                    if (counter_horizontal % 4 == 0)
-                    {
-                        result[i][j] = '.';
-                        counter_horizontal = 0;
-                    }
-                    else
-                    {
-                        result[i][j] = '#';
-                    }
-                }
-                counter_horizontal++;
-            }
-        }
-        counter_vertical++;
-    }
-    for (int i = 0; i < 2 * n; i++)
+    std::string marker = "AM";
+    if (hours < 12)
     {
-        for (int j = 0; j < 2 * n; j++)
-        {
-            std::cout << result[i][j];
-        }
-        std::cout << std::endl;
+        marker = "AM";
     }
+    else
+    {
+        marker = "PM";
+    }
+    // convert hours to clock format
+    if (hours == 0)
+    {
+        hours = 12;
+    }
+    else if (hours > 12)
+    {
+        hours -= 12;
+    }
+    char *new_line = new char[9];
+    sprintf(new_line, "%02d:%02d %s", hours, minutes, marker.c_str());
+    return new_line;
 }
+
 int main(int argc, char **argv)
 {
     int number_of_lines = 0;
     std::cin >> number_of_lines;
     int n;
-    int ns[number_of_lines];
+
+    std::vector<std::string> lines(number_of_lines);
+    std::string line;
     for (int i = 0; i < number_of_lines; i++)
     {
-        std::cin >> ns[i];
+        std::cin >> line;
+        lines[i] = line;
     }
     for (int i = 0; i < number_of_lines; i++)
     {
-        drawChessBoard(ns[i]);
+        std::cout << convertTime(lines[i].c_str()) << std::endl;
     }
+
     return 0;
 }
