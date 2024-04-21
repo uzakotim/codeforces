e8b75f80b1968d215fdfdd5a48e68ab21e7ae8e2
uzakotim <timauzakov@gmail.com>
2024-04-21 14:01:13 +0500

    upscaling

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,71 @@
 
 void drawChessBoard(int n)
 {
+    char result[2 * n][2 * n];
+    int counter_horizontal = 0;
+    int counter_vertical = 0;
+    for (int i = 0; i < 2 * n; i++)
+    {
+        counter_horizontal = 0;
+        if (counter_vertical % 4 == 0)
+            counter_vertical = 0;
+        // PRINT ROW
+        if (counter_vertical < 2)
+        {
+            for (int j = 0; j < 2 * n; j++)
+            {
+                if (counter_horizontal % 4 == 0)
+                {
+                    result[i][j] = '#';
+                    counter_horizontal = 0;
+                }
+                if (counter_horizontal < 2)
+                {
+                    result[i][j] = '#';
+                }
+                else
+                {
+
+                    result[i][j] = '.';
+                }
+                counter_horizontal++;
+            }
+        }
+        else
+        {
+            // PRINT REVERSE ROW
+            for (int j = 0; j < 2 * n; j++)
+            {
+                if (counter_horizontal < 2)
+                {
+                    result[i][j] = '.';
+                }
+                else
+                {
+
+                    if (counter_horizontal % 4 == 0)
+                    {
+                        result[i][j] = '.';
+                        counter_horizontal = 0;
+                    }
+                    else
+                    {
+                        result[i][j] = '#';
+                    }
+                }
+                counter_horizontal++;
+            }
+        }
+        counter_vertical++;
+    }
+    for (int i = 0; i < 2 * n; i++)
+    {
+        for (int j = 0; j < 2 * n; j++)
+        {
+            std::cout << result[i][j];
+        }
+        std::cout << std::endl;
+    }
 }
 int main(int argc, char **argv)
 {
